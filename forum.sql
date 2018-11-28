52cd81112d1b35b78b31302d23e29baa54ea7926
Seongmin Park <seongminnpark@gmail.com>
2018-11-28 03:45:31 -0500

    Add sql syntax

--- forum.sql
+++ forum.sql
@@ -2,98 +2,97 @@ drop database if exists test1;
 create database test1;
 use test1;
 
-drop table if exists admins;
-create table IF NOT EXISTS admins (
-	aid int(10) NOT NULL AUTO_INCREMENT,
-    username varchar(255) NOT NULL,
-    _password varchar(255) NOT NULL,
-    _email varchar(255) NOT NULL unique,
-    primary key (aid)
-    );
-
-
-drop table if exists users;
-create table IF NOT EXISTS users (
-	uid int(10) NOT NULL AUTO_INCREMENT,
-    username varchar(255) NOT NULL,
-    _password varchar(255) NOT NULL,
-    _email varchar(255) NOT NULL unique,
-    primary key (uid)
-    );
+drop table if exists `user`;
+create table IF NOT EXISTS `user` (
+	user_id int(10) NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL unique,
+    password_hash varchar(255) NOT NULL,
+    token varchar(255) NOT NULL,
+    primary key (user_id)
+);
     
+drop table if exists `user_role`;
+create table IF NOT EXISTS `user_role` (
+	role_id int(10) NOT NULL AUTO_INCREMENT,
+    role_name varchar(255) NOT NULL unique,
+    primary key (role_id)
+);
 
-drop table if exists topics;
-create table IF NOT EXISTS topics (
-    tid int(10) NOT NULL AUTO_INCREMENT,
-    topic varchar(255) NOT NULL unique,
-    _description varchar(255),
-    parent_id int(10),
-    primary key (tid)
-    );
+drop table if exists `user_is_role`;
+create table IF NOT EXISTS `user_is_role` (
+    user_id int(10) NOT NULL unique,
+    role_id int(10) NOT NULL,
+    primary key (user_id),
+    foreign key (user_id) references `user`(user_id),
+    foreign key (role_id) references `user_role`(role_id)
+);
+
+drop table if exists `category`;
+create table IF NOT EXISTS `category` (
+    category_id int(10) NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL unique,
+    primary key (category_id)
+);
     
-drop table if exists posts;
-create table IF NOT EXISTS posts (
-	pid int(10) NOT NULL AUTO_INCREMENT, 
-    uid int(10) NOT NULL,
-    tid int(10) NOT NULL,
+drop table if exists `post`;
+create table IF NOT EXISTS `post` (
+	post_id int(10) NOT NULL AUTO_INCREMENT, 
+    poster_id int(10) NOT NULL,
+    category_id int(10) NOT NULL,
     title  varchar(255) NOT NULL,
     content varchar(255) NOT NULL,
-    post_time timestamp default now(),
-    primary key (pid),
-    foreign key (uid) references users(uid),
-    foreign key (tid) references topics(tid)
-    );
+    post_time datetime default now(),
+    primary key (post_id),
+    foreign key (poster_id) references `user`(user_id),
+    foreign key (category_id) references `category`(category_id)
+);
     
-drop table if exists comments;
-create table IF NOT EXISTS comments (
-	cid int(10) NOT NULL AUTO_INCREMENT, 
-    uid int(10) NOT NULL,
-    pid int(10) NOT NULL,
-    father_id int(10),
+drop table if exists `comment`;
+create table IF NOT EXISTS `comment` (
+	comment_id int(10) NOT NULL AUTO_INCREMENT, 
+    commenter_id int(10) NOT NULL,
+    post_id int(10) NOT NULL,
     content varchar(255) NOT NULL,
     post_time datetime NOT NULL,
-    primary key (cid),
-    foreign key (uid) references users(uid),
-    foreign key (pid) references posts(pid) ON DELETE CASCADE,
-    foreign key (father_id) references comments(cid) ON DELETE CASCADE
-    );
+    primary key (comment_id),
+    foreign key (commenter_id) references `user`(user_id),
+    foreign key (post_id) references `post`(post_id) ON DELETE CASCADE
+);
 
-drop table if exists bans;
-create table IF NOT EXISTS bans (
-    bid int(10) NOT NULL AUTO_INCREMENT, 
-	uid int(10) NOT NULL unique,
-    post_time datetime NOT NULL,
-    aid int(10) NOT NULL,
-    content varchar(255) NOT NULL,
-    primary key (bid),
-    foreign key (uid) references users(uid),
-	foreign key (aid) references admins(aid)
-    );
+drop table if exists `likes`;
+create table IF NOT EXISTS `likes` (
+	likes_id int(10) NOT NULL auto_increment,
+	user_id int(10) NOT NULL,
+    post_id int(10) NOT NULL,
+	like_time datetime NOT NULL,
+    primary key (likes_id),
+    unique(user_id, post_id),
+    foreign key (post_id) references post(post_id) ON DELETE CASCADE,
+    foreign key (user_id) references user(user_id)
+);
 
-drop table if exists likes;
-create table IF NOT EXISTS likes (
-	lid int(10) NOT NULL auto_increment,
-	uid int(10) NOT NULL,
-    pid int(10) NOT NULL,
-	post_time datetime NOT NULL,
-    primary key (lid),
-    unique(uid, pid),
-    foreign key (pid) references posts(pid) ON DELETE CASCADE,
-    foreign key (uid) references users(uid)
-    );
+drop table if exists `ban`;
+create table IF NOT EXISTS `ban` (
+    ban_id int(10) NOT NULL AUTO_INCREMENT, 
+	banned_id int(10) NOT NULL unique,
+    banner_id int(10) NOT NULL,
+    ban_time datetime NOT NULL,
+    primary key (ban_id),
+    foreign key (banned_id) references `user`(user_id),
+	foreign key (banner_id) references `user`(user_id)
+);
 
-drop table if exists reports;
-create table IF NOT EXISTS reports (
-	rid int(10) NOT NULL AUTO_INCREMENT,
+drop table if exists `report`;
+create table IF NOT EXISTS `report` (
+	report_id int(10) NOT NULL AUTO_INCREMENT,
 	reporter int(10) NOT NULL,
     reported int (10) NOT NULL,
-    pid int(10) ,
-	cid int(10) , 
-	post_time datetime NOT NULL,
-    primary key (rid),
-    foreign key (reporter) references users(uid),
-    foreign key (reported) references users(uid),
-    foreign key (pid) references posts(pid) ON DELETE CASCADE,
-	foreign key (cid) references comments(cid) ON DELETE CASCADE
-    );
+    post_id int(10) ,
+	reoport_time datetime NOT NULL,
+    comments TEXT,
+    primary key (report_id),
+    foreign key (reporter) references `user`(user_id),
+    foreign key (reported) references `user`(user_id),
+    foreign key (post_id) references `post`(post_id) ON DELETE CASCADE
+);
  
